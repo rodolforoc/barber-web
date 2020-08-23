d8e8faf0fceeeaf1752701a58b946dc9b7791c9b
Rodolfo Caetano <rodolforoc97@gmail.com>
2020-08-22 22:33:05 -0300

    Add SignUp page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -7,16 +7,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logoImg} alt="logo" />
 
       <form>
-        <h1> Faça seu logon</h1>
+        <h1> Faça seu cadastro</h1>
 
-        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-
+        <Input name="nome" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
         <Input
           name="password"
           icon={FiLock}
@@ -24,19 +25,15 @@ const SignIn: React.FC = () => (
           placeholder="Senha"
         />
 
-        <Button type="submit">Entrar</Button>
-
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastro</Button>
       </form>
 
       <a href="criar">
-        <FiLogIn />
-        Criar minha conta
+        <FiArrowLeft />
+        Voltar para logon
       </a>
     </Content>
-
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
